fef8626cec8d5f8e7a9999c7540b5ad215faf97c
Jessi Shamis <92047551+shamis15@users.noreply.github.com>
2022-06-09 12:58:30 -0400

    Update test.py

--- myproject/test.py
+++ myproject/test.py
@@ -12,7 +12,14 @@ st.error('Error message')
 st.warning('Warning message')
 st.info('Info message')
 
+
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
 st.success('success message')
+
+
 # Two equal columns: 
 col1, col2 = st.columns(2)
 col1.write("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
